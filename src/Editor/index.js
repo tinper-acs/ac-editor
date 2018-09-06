edf3e2cdefd034a4db90bb0c180f12e13c66957b
kvkens <kvkens@qq.com>
2018-09-06 11:34:08 +0800

    rm:docs

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -16,8 +16,16 @@ class Editor extends Component {
     }
   }
 
+  componentDidMount () {
+    // 假设此处从服务端获取html格式的编辑器内容
+    const htmlContent = `<p>你好</p>`;
+    // 使用EditorState.createFrom将html字符串转换为编辑器需要的editorState数据
+    this.setState({
+      editorState: EditorState.createFrom(htmlContent)
+    })
+  }
 
-  submitContent() {
+  submitContent = () => {
     // 在编辑器获得焦点时按下ctrl+s会执行此方法
     // 编辑器内容提交到服务端之前，可直接调用editorState.toHTML()来获取HTML格式的内容
     const htmlContent = this.state.editorState.toHTML()
